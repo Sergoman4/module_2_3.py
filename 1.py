6bc84f412029152416b82ae22d96a7cea820b466
Sergoman4 <28062002sergey@gmail.com>
2024-06-25 14:05:26 +1000

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,8 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+print(my_list)
+while True:
+  number = int(input('ведите число: '))
+  if number < 0:
+    break
+  elif number >= 0:
+    print(number)
