9ed6d8bc6baa981a104fba6d8937c089bf3c5731
Sergoman4 <28062002sergey@gmail.com>
2024-06-25 23:04:39 +1000

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,7 +2,7 @@ my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 print(my_list)
 while True:
   number = int(input('ведите число: '))
-  if number < 0:
+  if number <= 0:
     break
-  elif number >= 0:
+  elif number > 0:
     print(number)
